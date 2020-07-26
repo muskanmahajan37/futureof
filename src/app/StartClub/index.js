f2581686eea581b1f31cf882e86dae0895443e4b
royal2710 <royal2710@outlook.com>
2020-07-26 22:16:24 +0800

    club setup complete

--- src/app/StartClub/index.js
+++ src/app/StartClub/index.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import './start-club.scss';
 
 function StartClub() {
+    const [btnBannerImage, SetBtnBannerImage] = useState(null);
+    const [btnMobileIcon, SetBtnMobileIcon] = useState(null);
     const [step, setStep] = useState(0);
 
   return (
@@ -11,46 +13,46 @@ function StartClub() {
                 <div className={step === 0 ? "form-first" : "form-first hidden"}>
                     <h4>CLUB SETUP</h4>
                     <div className="form-group mt-4">
-                        <label>CLUB NAME</label>
+                        <label className="text-secondary">CLUB NAME</label>
                         <input className="form-control" />
                     </div>
                     <div className="form-group mt-4">
-                        <label>CLUB NAME</label>
-                        <small>Describe your Club and set the tone and expectations for members</small>
+                        <label className="text-secondary">CLUB NAME</label>
+                        <small className="text-secondary">Describe your Club and set the tone and expectations for members</small>
                         <textarea className="form-control" rows="5"></textarea>
                     </div>
                     <div className="form-group mt-4">
-                        <label>CLUB INTERESTS(ADD UP TO 3)</label>
+                        <label className="text-secondary">CLUB INTERESTS(ADD UP TO 3)</label>
                         <div className="row py-1">
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />RENEWABLE ENERGY
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />RENEWABLE ENERGY</label>
                             </div>
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />OIL AND GAS
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />OIL AND GAS</label>
                             </div>
                         </div>
                         <div className="row py-1">
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />AUTONOMOUS VEHICLES
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />AUTONOMOUS VEHICLES</label>
                             </div>
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />CLEAN TECHNOLOGY
-                            </div>
-                        </div>
-                        <div className="row py-1">
-                            <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />AI IN ENERGY
-                            </div>
-                            <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />FUEL CELL TECHNOLOGY
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />CLEAN TECHNOLOGY</label>
                             </div>
                         </div>
                         <div className="row py-1">
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />SOLAR POWER
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />AI IN ENERGY</label>
                             </div>
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <input type="checkbox" className="mr-2" />WIND POWER
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />FUEL CELL TECHNOLOGY</label>
+                            </div>
+                        </div>
+                        <div className="row py-1">
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6">
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />SOLAR POWER</label>
+                            </div>
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6">
+                                <label className="d-flex text-secondary"><input type="checkbox" className="form-control mr-2" />WIND POWER</label>
                             </div>
                         </div>
                     </div>
@@ -64,23 +66,28 @@ function StartClub() {
                 <div className={step === 1 ? "form-second" : "form-second hidden"}>
                 <h4>CLUB SETUP</h4>
                     <div className="form-group mt-4">
-                        <label>CLUB DUES</label>
-                        <input type="checkbox" /> <span>$19</span>
+                        <label className="text-secondary">CLUB DUES</label>
+                        <br />
+                        <label>
+                            <input type="checkbox" className="mt-3 mr-4" /><span className="badge badge-primary"><sup>$</sup>19</span>
+                        </label>
                     </div>
                     <div className="form-group mt-4">
-                        <label>SET PRIMARY TIMEZONE</label>
-                        <input type="text" className="form-control" />
+                        <label className="text-secondary">SET PRIMARY TIMEZONE</label>
+                        <input type="text" className="form-control text-secondary" />
                     </div>
                     <div className="form-group mt-4">
-                        <label>CLUB INTERESTS(ADD UP TO 3)</label>
+                        <label className="text-secondary">CLUB INTERESTS(ADD UP TO 3)</label>
                         <div className="row py-1">
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <label>UPLOAD A BANNER IMAGE FOR THE CLUB</label>
-                                <input type="file" className="mr-2" />
+                                <label className="text-secondary pr-5">UPLOAD A BANNER IMAGE FOR THE CLUB</label>
+                                <input type="file" className="form-control hidden" ref={input => SetBtnBannerImage(input)} />
+                                <button type="button" className="btn btn-upload" onClick={() => btnBannerImage.click()}></button>
                             </div>
                             <div className="col-6 col-sm-6 col-md-6 col-lg-6">
-                                <label>UPLOAD AN ICON IMAGE FOR THE MOBILE APP</label>
-                                <input type="file" className="mr-2" />
+                                <label className="text-secondary pr-5">UPLOAD AN ICON IMAGE FOR THE MOBILE APP</label>
+                                <input type="file" className="form-control hidden" ref={input => SetBtnMobileIcon(input)} />
+                                <button type="button" className="btn btn-upload" onClick={() => btnMobileIcon.click()}></button>
                             </div>
                         </div>
                     </div>
@@ -94,10 +101,10 @@ function StartClub() {
                 <div className={step === 2 ? "form-third" : "form-third hidden"}>
                 <h4>CLUB SETUP</h4>
                     <div className="form-group mt-4">
-                        <label>YOUR CLUB SETUP IS ALMOST COMPLETE:</label>
-                        <label>HERE IS A LINK TO YOUR MEMBERSHIP LANDING PAGE. YOU MAY COPY THIS AND SEND THIS TO PROSPECTIVE MEMBERS.</label>
-                        <input className="form-control" placeholder="https://" />
-                        <label>VERIFICATION BY YOU OR AUTHO CO-MANAGER TO JOIN THE CLUB IS REQUIRED TO COMPLETE THE MEMBERSHIP REQUEST.</label>
+                        <label className="mt-4 text-secondary">YOUR CLUB SETUP IS ALMOST COMPLETE:</label>
+                        <label className="mt-4 text-secondary">HERE IS A LINK TO YOUR MEMBERSHIP LANDING PAGE. YOU MAY COPY THIS AND SEND THIS TO PROSPECTIVE MEMBERS.</label>
+                        <input className="form-control text-primary mt-4" placeholder="https://" />
+                        <label className="mt-4 text-secondary">VERIFICATION BY YOU OR AUTHO CO-MANAGER TO JOIN THE CLUB IS REQUIRED TO COMPLETE THE MEMBERSHIP REQUEST.</label>
                     </div>
                     <button 
                         type="button"
